9780a83bb970df04c66a56173544e648ff7abb95
Remy349 <santoraga15@gmail.com>
2024-01-24 22:15:38 -0600

    First commit and Initial files added.

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <h1 className='font-bold text-3xl'>OK!!!</h1>
+  </React.StrictMode>,
+)
